2fc63081cfd2b88f10bc8edcbef7da2442d91bdf
Kushal23m <kushalgowda2310@gmail.com>
2025-11-10 21:30:04 +0530

    server schema

---
+++ schema.js
@@ -0,0 +1,22 @@
+// schema for server side validation using Joi -> without this encoding and decoding form fields can be bypassed or submitted using tools like Hopscotch, Postman etc.
+
+const Joi = require('joi');
+
+module.exports.encodeSchema = Joi.object({
+    Encode: Joi.object({
+        title: Joi.string().required().max(100),
+        message: Joi.string().required().max(500),
+        email: Joi.string().required().email(),
+        otp: Joi.string().required().length(6),
+        password: Joi.string().required().min(5).max(20),
+    }).required()
+});
+
+module.exports.decodeSchema = Joi.object({
+    Decode: Joi.object({
+        email: Joi.string().required().email(),
+        password: Joi.string().required().min(5).max(20),
+        otp: Joi.string().length(6),
+        encryptionId: Joi.string()
+    }).required()
+});
